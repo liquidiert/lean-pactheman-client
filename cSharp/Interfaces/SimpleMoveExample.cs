6bed998ec172aabd76eda44220f4591aaa375eef
Korbinian Habereder <haberederkorbinian@googlemail.com>
2021-01-25 14:57:55 +0100

    intermediate commit
    - fixed connection

--- cSharp/Interfaces/SimpleMoveExample.cs
+++ cSharp/Interfaces/SimpleMoveExample.cs
@@ -1,9 +1,17 @@
 using PacTheMan.Models;
+using System;
+using System.Collections.Generic;
 
 namespace lean_pactheman_client {
     public static class SimpleMoveExample {
         public static Position PerformMove(Player player) {
-            return player.Position;
+            var velocities = new List<Position>().AddMany(
+                    new Position { X = -64, Y = 0 }, // left
+                    new Position { X = 64, Y = 0 }, // right
+                    new Position { X = 0, Y = 64 }, // up
+                    new Position { X = 0, Y = -64 } // down
+                );
+            return player.Position.AddOther(velocities[new Random().Next(velocities.Count)]);
         }
     }
 }
